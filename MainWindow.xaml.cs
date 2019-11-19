ba7cf3fd7f959cb56b6b1fcc753a7cc9770ded22
Andreas Carlsson <andreas95carlsson@gmail.com>
2019-11-19 10:31:34 +0100

    Updated to calc on string

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -21,150 +21,86 @@ namespace TestWPF
     public partial class MainWindow : Window
     {
 
-        decimal num1 = 0;
-        decimal num2 = 0;
+        private double total1 = 0;
+        private double total2 = 0;
 
-        string operation = "";
+        bool plusButtonClicked = false;
+        bool minusButtonClicked = false;
+        bool multiplyButtonClicked = false;
+        bool divideButtonClicked = false;
 
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void numAppend(int btnNum)
+        private void btnPlus_Click(object sender, RoutedEventArgs e)
         {
+            total1 += double.Parse(txtDisplay.Text);
+            labelCurrentOperation.Content = txtDisplay.Text + "+";
+            txtDisplay.Text = "";
 
-            if (operation == "")
-            {
-                num1 = (num1 * 10) + btnNum;
-                txtDisplay.Text = num1.ToString();
-                labelCurrentOperation.Content = num1;
-            }
-            else
-            {
-                num2 = (num2 * 10) + btnNum;
-                txtDisplay.Text = num2.ToString();
-                labelCurrentOperation.Content = num1 + operation + num2;
-            }
+            plusButtonClicked = true;
+            minusButtonClicked = false;
+            multiplyButtonClicked = false;
+            divideButtonClicked = false;
         }
-        private void btn0_Click(object sender, RoutedEventArgs e)
+        private void btnMin_Click(object sender, RoutedEventArgs e)
         {
-            numAppend(0);
-        }
+            total1 += double.Parse(txtDisplay.Text);
+            labelCurrentOperation.Content = txtDisplay.Text + "-";
+            txtDisplay.Text = "";
 
-        private void btn1_Click(object sender, RoutedEventArgs e)
-        {
-            numAppend(1);
+            plusButtonClicked = false;
+            minusButtonClicked = true;
+            multiplyButtonClicked = false;
+            divideButtonClicked = false;
         }
-
-        private void btn2_Click(object sender, RoutedEventArgs e)
+        private void btnTimes_Click(object sender, RoutedEventArgs e)
         {
-            numAppend(2);
+            total1 += double.Parse(txtDisplay.Text);
+            labelCurrentOperation.Content = txtDisplay.Text + "*";
+            txtDisplay.Text = "";
+
+            plusButtonClicked = false;
+            minusButtonClicked = false;
+            multiplyButtonClicked = true;
+            divideButtonClicked = false;
         }
-
-        private void btn3_Click(object sender, RoutedEventArgs e)
+        private void btnDivide_Click(object sender, RoutedEventArgs e)
         {
-            numAppend(3);
-        }
+            total1 += double.Parse(txtDisplay.Text);
+            labelCurrentOperation.Content = txtDisplay.Text + "/";
+            txtDisplay.Text = "";
 
-        private void btn4_Click(object sender, RoutedEventArgs e)
-        {
-            numAppend(4);
-        }
-
-        private void btn5_Click(object sender, RoutedEventArgs e)
-        {
-            numAppend(5);
-        }
-
-        private void btn6_Click(object sender, RoutedEventArgs e)
-        {
-            numAppend(6);
-        }
-
-        private void btn7_Click(object sender, RoutedEventArgs e)
-        {
-            numAppend(7);
-        }
-
-        private void btn8_Click(object sender, RoutedEventArgs e)
-        {
-            numAppend(8);
-        }
-
-        private void btn9_Click(object sender, RoutedEventArgs e)
-        {
-            numAppend(9);
+            plusButtonClicked = false;
+            minusButtonClicked = false;
+            multiplyButtonClicked = false;
+            divideButtonClicked = true;
         }
 
         private void btnCE_Click(object sender, RoutedEventArgs e)
         {
-            if (operation == "")
-            {
-                num1 = 0;
-                txtDisplay.Text = "0";
-                labelCurrentOperation.Content = "";
-            }
-            else
-            {
-                num2 = 0;
-                txtDisplay.Text = num1.ToString();
-                labelCurrentOperation.Content = num1;
-            }
-            operation = "";
+            txtDisplay.Text = "0";
+            labelCurrentOperation.Content = "";
         }
 
         private void btnC_Click(object sender, RoutedEventArgs e)
         {
-            operation = "c";
             txtDisplay.Text = "0";
-            num1 = 0;
-            num2 = 0;
-            operation = "";
-
             labelCurrentOperation.Content = "";
         }
-
-        private void btnPlus_Click(object sender, RoutedEventArgs e)
-        {
-            operation = "+";
-            txtDisplay.Text = "+";
-
-            labelCurrentOperation.Content = num1 + operation;
-        }
-
-        private void btnMin_Click(object sender, RoutedEventArgs e)
-        {
-            operation = "-";
-            txtDisplay.Text = "-";
-            labelCurrentOperation.Content = num1 + operation;
-        }
-
-        private void btnTimes_Click(object sender, RoutedEventArgs e)
-        {
-            operation = "*";
-            txtDisplay.Text = "*";
-            labelCurrentOperation.Content = num1 + operation;
-        }
-
-        private void btnDivide_Click(object sender, RoutedEventArgs e)
-        {
-            operation = "/";
-            txtDisplay.Text = "/";
-            labelCurrentOperation.Content = num1 + operation;
-        }
-
         private void btnPlusMin_Click(object sender, RoutedEventArgs e)
         {
-            if (operation == "")
+            if (txtDisplay.Text.StartsWith("-"))
             {
-                num1 = num1 * -1;
-                txtDisplay.Text = num1.ToString();
+                //It's negative now, so strip the `-` sign to make it positive
+                txtDisplay.Text = txtDisplay.Text.Substring(1);
             }
-            else
+            else if (!string.IsNullOrEmpty(txtDisplay.Text) && decimal.Parse(txtDisplay.Text) != 0)
             {
-                num2 = num2 * -1;
-                txtDisplay.Text = num2.ToString();
+                //It's positive now, so prefix the value with the `-` sign to make it negative
+                txtDisplay.Text = "-" + txtDisplay.Text;
             }
         }
 
@@ -175,74 +111,89 @@ namespace TestWPF
 
         private void btnBack_Click(object sender, RoutedEventArgs e)
         {
-
-            if (operation == "")
-            {
-                txtDisplay.Text = txtDisplay.Text.Remove(txtDisplay.Text.Length - 1, 1);
-                num1 = decimal.Parse(num1.ToString().Remove(num1.ToString().Length - 1, 1));
-
-                labelCurrentOperation.Content = labelCurrentOperation.Content.ToString().Remove(labelCurrentOperation.Content.ToString().Length - 1, 1);
-            }
-            else
-            {
-                txtDisplay.Text = txtDisplay.Text.Remove(txtDisplay.Text.Length - 1, 1);
-                num2 = decimal.Parse(num2.ToString().Remove(num2.ToString().Length - 1, 1));
-
-                labelCurrentOperation.Content = labelCurrentOperation.Content.ToString().Remove(labelCurrentOperation.Content.ToString().Length - 1, 1);
-            }
-
-        }
-
-        private void txtDisplay_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
+            txtDisplay.Text = txtDisplay.Text.Substring(0, txtDisplay.Text.Length - 1);
         }
 
         private void btnEquals_Click(object sender, RoutedEventArgs e)
         {
-            switch (operation)
+            if (plusButtonClicked == true)
             {
-                case "+":
-                    txtDisplay.Text = (num1 + num2).ToString();
-                    labelCurrentOperation.Content = num1 + operation + num2;
-
-                    break;
-
-                case "c":
-                    txtDisplay.Text = "0";
-
-                    break;
-
-                case "-":
-                    txtDisplay.Text = (num1 - num2).ToString();
-                    labelCurrentOperation.Content = num1 + operation + num2;
-                    break;
-
-                case "*":
-                    txtDisplay.Text = (num1 * num2).ToString();
-                    labelCurrentOperation.Content = num1 + operation + num2;
-                    break;
-
-                case "/":
-                    txtDisplay.Text = (num1 / num2).ToString();
-                    labelCurrentOperation.Content = num1 + operation + num2;
-                    break;
-
-                case "ce":
-                    txtDisplay.Text = num2.ToString();
-                    break;
-
-                default:
-                    break;
-
+                total2 = total1 + double.Parse(txtDisplay.Text);
+                labelCurrentOperation.Content = total1 + "+" + double.Parse(txtDisplay.Text);
+                txtDisplay.Text = total2.ToString();
+                total1 = 0;
             }
-
-            num1 = 0;
-            num2 = 0;
-            
-            operation = "";
-
+            else if (minusButtonClicked == true)
+            {
+                total2 = total1 - double.Parse(txtDisplay.Text);
+                labelCurrentOperation.Content = total1 + "-" + double.Parse(txtDisplay.Text);
+                txtDisplay.Text = total2.ToString();
+                total1 = 0;
+            }
+            else if (multiplyButtonClicked == true)
+            {
+                total2 = total1 * double.Parse(txtDisplay.Text);
+                labelCurrentOperation.Content = total1 + "*" + double.Parse(txtDisplay.Text);
+                txtDisplay.Text = total2.ToString();
+                total1 = 0;
+            }
+            else if (divideButtonClicked == true)
+            {
+                total2 = total1 / double.Parse(txtDisplay.Text);
+                labelCurrentOperation.Content = total1 + "/" + double.Parse(txtDisplay.Text);
+                txtDisplay.Text = total2.ToString();
+                total1 = 0;
+            }
         }
-
+        private void btn0_Click(object sender, RoutedEventArgs e)
+        {
+            txtDisplay.Text += "0";
+            labelCurrentOperation.Content = txtDisplay.Text;
+        }
+        private void btn1_Click(object sender, RoutedEventArgs e)
+        {
+            txtDisplay.Text += "1";
+            labelCurrentOperation.Content = txtDisplay.Text;
+        }
+        private void btn2_Click(object sender, RoutedEventArgs e)
+        {
+            txtDisplay.Text += "2";
+            labelCurrentOperation.Content = txtDisplay.Text;
+        }
+        private void btn3_Click(object sender, RoutedEventArgs e)
+        {
+            txtDisplay.Text += "3";
+            labelCurrentOperation.Content = txtDisplay.Text;
+        }
+        private void btn4_Click(object sender, RoutedEventArgs e)
+        {
+            txtDisplay.Text += "4";
+            labelCurrentOperation.Content = txtDisplay.Text;
+        }
+        private void btn5_Click(object sender, RoutedEventArgs e)
+        {
+            txtDisplay.Text += "5";
+            labelCurrentOperation.Content = txtDisplay.Text;
+        }
+        private void btn6_Click(object sender, RoutedEventArgs e)
+        {
+            txtDisplay.Text += "6";
+            labelCurrentOperation.Content = txtDisplay.Text;
+        }
+        private void btn7_Click(object sender, RoutedEventArgs e)
+        {
+            txtDisplay.Text += "7";
+            labelCurrentOperation.Content = txtDisplay.Text;
+        }
+        private void btn8_Click(object sender, RoutedEventArgs e)
+        {
+            txtDisplay.Text += "8";
+            labelCurrentOperation.Content = txtDisplay.Text;
+        }
+        private void btn9_Click(object sender, RoutedEventArgs e)
+        {
+            txtDisplay.Text += "9";
+            labelCurrentOperation.Content = txtDisplay.Text;
+        }
     }
 }
